23d8738e8c99e646685b4f89ece1478416ca2583
Ivan <kosarevsky2000@yandex.ru>
2023-08-13 10:50:57 +0300

    initial commit

---
+++ HomeWorkAdv-1-1/HomeWorkAdv-1-1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+	std::vector<int> numbers{ 4, 7, 9, 14, 12 };
+	for (auto& i : numbers) {
+		std::cout << i << " ";
+	}
+	std::cout << std::endl;
+	auto func = [&numbers]() {
+		for (auto& i : numbers) {
+			if (i % 2 != 0) {
+				i *= 3;
+			}
+			std::cout << i << " ";
+		}
+	};
+	func();
+}
+
